d314b2009ca3586685b0c7721f3181821c7063e1
basmalahstore <106342606+basmalahstore@users.noreply.github.com>
2022-05-26 18:23:23 +0000

    Add files via upload

---
+++ assets/js/slick.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.home__img-bg').slick({
+        // dots: true,
+        infinite: true,
+        // speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+        pauseOnFocus: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+});
